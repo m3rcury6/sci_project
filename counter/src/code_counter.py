8971ceb280aa9ac33285c076ecfb16c394ba7e6c
kris <kg-171971@hs-weingarten.de>
2018-09-12 14:57:27 +0200

    udpated to both know where program is in directory, as well as write out to a directory

--- counter/src/code_counter.py
+++ counter/src/code_counter.py
@@ -11,6 +11,24 @@ import time
 from sys import argv
 from std_msgs.msg import String
 from std_msgs.msg import UInt8
+import os
+
+# 1.0: Ensure write path is available ##########################################
+write_path = argv[1]
+print 'path:'
+print write_path
+if(not os.path.exists(write_path)):
+	print "error, path below doesn't exist. creating..."
+	print write_path
+	os.mkdir(write_path)
+os.chdir(write_path)
+print 'currently located at:',os.getcwd()
+
+# 2.0: create write file (csv) in order to log detections ######################
+fname = "out_"+str(time.time())+".csv"
+fout = file(fname,'w')
+fout.write('colA,colB')
+fout.close()
 
 def call_inc(data):
 	print "i heard something",time.time()
