2c5a29f0bc9a84b47e7f058a6dbaecb0084f32f5
Nitin Nayak <nn-171983@hs-weingarten.de>
2018-09-13 16:36:09 +0200

    Working image input node

--- image_input/src/image_input.py
+++ image_input/src/image_input.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 '''
-Objective: Publish images from folder
-Authors: Nitin Nayak
+Objective: Publish images from folder to a ROS topic
+Author: Nitin Nayak
 '''
 
 import rospy
@@ -11,10 +11,12 @@ from sys import argv
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
-#from PIL import Image
 import os
-import numpy as np
 import cv2
+
+rospy.init_node('image_input')
+pub_image = rospy.Publisher('images', Image, queue_size=10)
+pub_image_name = rospy.Publisher('image_name', String, queue_size=10)
 
 # 1.0: Ensure read path is available ##########################################
 read_path = argv[1]
@@ -23,8 +25,6 @@ print read_path
 if(not os.path.exists(read_path)):
 	print "error, path below doesn't exist. creating..."
 	print 'currently located at:',os.getcwd()
-	#print read_path
-	#os.mkdir(read_path)
 os.chdir(read_path)
 print 'currently located at : ',os.getcwd()
 
@@ -41,19 +41,16 @@ for filename in os.listdir(read_path):
 # 3.0: appends the images and displays them
 for val in range(len(image_list)):
 	image.append(cv2.imread(str(image_list[val]), 1))
+	'''
+	TO VIEW EACH IMAGE remove this comment
 	cv2.imshow(str(image_list[val]),image[val])
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
+	'''
 
-
-rospy.init_node('image_input')
-pub_image = rospy.Publisher('images', Image, queue_size=10)
-pub_image_name = rospy.Publisher('image_name', String, queue_size=10)
-
-
-val = 0	#kills node once finished with all images
+val = 0	#kills ROS node once finished with all images
 while(not rospy.is_shutdown() and val < len(image_list)):
 	pub_image.publish(CvBridge().cv2_to_imgmsg(image[val], "bgr8"))
 	pub_image_name.publish('Current image: ' + str(image_list[val]))
-	time.sleep(1)
-	val = val + 1
+	time.sleep(0.01)
+	val += 1
