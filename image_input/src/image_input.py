6b67511f898642c07dd12a4c7bf34ad8efc0035a
Nitin Nayak <nn-171983@hs-weingarten.de>
2018-09-13 16:17:17 +0200

    Multiple images being published. ROS Node ends on finishing all images. Code cleanup needed

--- image_input/src/image_input.py
+++ image_input/src/image_input.py
@@ -9,6 +9,9 @@ import rospy
 import time
 from sys import argv
 from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+#from PIL import Image
 import os
 import numpy as np
 import cv2
@@ -23,17 +26,34 @@ if(not os.path.exists(read_path)):
 	#print read_path
 	#os.mkdir(read_path)
 os.chdir(read_path)
-print 'currently located at:',os.getcwd()
+print 'currently located at : ',os.getcwd()
 
-# 2.0: display image ######################
-img =  cv2.imread('00001.jpg', 1)
-cv2.imshow('image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# 2.0: list images ######################
+image_list = []
+image = []
+valid_images = [".jpg"]
+for filename in os.listdir(read_path):
+	ext = os.path.splitext(filename)[1]
+	if ext.lower() not in valid_images:
+		continue
+	image_list.append(filename)
+
+# 3.0: appends the images and displays them
+for val in range(len(image_list)):
+	image.append(cv2.imread(str(image_list[val]), 1))
+	cv2.imshow(str(image_list[val]),image[val])
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
 
 rospy.init_node('image_input')
-pub_txt = rospy.Publisher('image_input',String,queue_size=10)
+pub_image = rospy.Publisher('images', Image, queue_size=10)
+pub_image_name = rospy.Publisher('image_name', String, queue_size=10)
 
-while(not rospy.is_shutdown()):
-	pub_txt.publish('Publishing image'+str(time.time()))
+
+val = 0	#kills node once finished with all images
+while(not rospy.is_shutdown() and val < len(image_list)):
+	pub_image.publish(CvBridge().cv2_to_imgmsg(image[val], "bgr8"))
+	pub_image_name.publish('Current image: ' + str(image_list[val]))
 	time.sleep(1)
+	val = val + 1
