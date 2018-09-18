2b3c1231137645b8c5f3654aa02538a494bdce44
FSTW Dell <fstw.dv>
2018-09-18 15:29:03 +0200

    quick test to ensure that able to import a module from another ROS package. success.

--- talker/src/code_talker.py
+++ talker/src/code_talker.py
@@ -8,10 +8,16 @@ import rospy
 import time
 from sys import argv
 from std_msgs.msg import String
+import common_tools.lib_tools as lib
 
 rospy.init_node('args_node')
 pub_txt = rospy.Publisher('talk_node',String,queue_size=10)
+a=3.0
+b=4.0
+
 
 while(not rospy.is_shutdown()):
-	pub_txt.publish('hello world'+str(time.time()))
-	time.sleep(1)
+    print (a*a+b*b)**0.5
+    print 'new:',lib.pyt([a,b])
+    pub_txt.publish('hello world'+str(time.time()))
+    time.sleep(1)
