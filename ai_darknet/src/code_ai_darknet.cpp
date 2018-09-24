421152a2febe7d994171d563721ffeda683d64ab
kris <kg-171971@hs-weingarten.de>
2018-09-24 17:15:36 +0200

    now have a cpp node that can accept arguments and has a publisher. next up are subscriber for images, and output with string.

--- ai_darknet/src/code_ai_darknet.cpp
+++ ai_darknet/src/code_ai_darknet.cpp
@@ -2,34 +2,45 @@
 Authors: Kris Gonzalez, Nitin Nayak
 Objective: have simple c++ talker / listener node
 
+How to run (after compiling with catkin_make):
+>> roslaunch ai_darknet ai_darknet.launch
 
 Task List:
 STATUS | DESCRIPTION
 =======|========================
 
-
-
-
 */
 
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-
 #include <sstream>
 
-int main(int argc, char **argv){
-  ros::init(argc,argv,"talker");
+void chatter_Callback(const std_msgs::String::ConstPtr& msg){
+	ROS_INFO("I heard: [%s]",msg->data.c_str());
+} //void chatter_Callback
+
+int main(int argc, char **argv){ //argc / argv enable input arguments
+
+  //input argument review
+  std::cout << "Roslaunch arguments ("<< argc-3 <<"):\n";
+  for(int i=1;i<argc-2;i++){
+    std::cout << argv[i]<<"\n";
+  }//forloop
+  std::cout<<"\n";
+
+  // initialize node, etc.
+  ros::init(argc,argv,"ai_darknet_node");
   ros::NodeHandle n;
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter",1000); //change "change_pub" later
+  ros::Publisher pub_str = n.advertise<std_msgs::String>("chatter",1000);
   ros::Rate loop_rate(10);
   int count = 0;
-  while(ros::ok()){
+  while(ros::ok() && count < 5){
     std_msgs::String msg;
     std::stringstream ss;
     ss << "hello world " << count;
     msg.data = ss.str();
     ROS_INFO("%s",msg.data.c_str());
-    chatter_pub.publish(msg);
+    pub_str.publish(msg);
     ros::spinOnce();
     loop_rate.sleep();
     ++count;
