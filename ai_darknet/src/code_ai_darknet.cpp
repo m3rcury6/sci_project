d41e67d3b689209989d23c8429ce072632757097
kris <kg-171971@hs-weingarten.de>
2018-09-25 15:17:49 +0200

    now have node that is both receiving an image / name and transmitting a dummy response. next step is to integrate AI

--- ai_darknet/src/code_ai_darknet.cpp
+++ ai_darknet/src/code_ai_darknet.cpp
@@ -11,7 +11,7 @@ STATUS | DESCRIPTION
 
 
 General Steps of program:
-1. intialize ros node and all functions
+1. intialize ros node and all functions done
 2. listen for incoming image
 3. analyze image for cones
 4. return in string the name and number of cones detected
@@ -27,18 +27,37 @@ General Steps of program:
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <ros/console.h>
+#include <string>
+#include <iostream>
 
 //KJG_Sep24: was originally "CDarknet.h", but changing to "darknet.h" worked...
 #include "darknet.h" //requires change in CMakeLists.txt file
 
+ros::Publisher pub_res; // need to declare here to use in functions
+std::string IMG_NAME ("empty");
+//char* IMG_NAME;
 
-void chatter_Callback(const std_msgs::String::ConstPtr& msg){
-	ROS_INFO("I heard: [%s]",msg->data.c_str());
-} //void chatter_Callback
+void call_name(const std_msgs::String::ConstPtr& msg){
+  IMG_NAME = msg->data.c_str();
+//  ROS_INFO("call_name: %s",msg->data.c_str());
+//  std::cout << "in string:"<<IMG_NAME<<"\n";
+}// void call_name
 
-void imageCallback(const sensor_msgs::ImageConstPtr& msg){
+void call_image(const sensor_msgs::ImageConstPtr& msg){
   try{
-		ROS_INFO("I heard something...\n");
+    ROS_INFO("recieved: %s",IMG_NAME.c_str());
+
+    // here, should try out an algorithm
+
+
+
+    // once complete, algorithm returns an integer value
+    int res = 4; //this would be the result after img processing has happened
+		IMG_NAME = IMG_NAME + ","+std::to_string(res);
+		std_msgs::String msg_res;
+		msg_res.data = IMG_NAME;
+		pub_res.publish(msg_res);
+
 		// pDarknet->SetInput(cv_bridge::toCvShare(msg, "rgb8")->image);
     // ConeDetection::bbox_array oMessage = pDarknet->Forward();
     // oMessage.header.stamp = msg->header.stamp;
@@ -56,8 +75,7 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg){
 		ROS_ERROR("kjgnote: somehow ended up here while debugging...");
     ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
   }//catch
-}//void imageCallback
-
+}//void call_image
 
 
 int main(int argc, char **argv){ //argc / argv enable input arguments
@@ -73,19 +91,26 @@ int main(int argc, char **argv){ //argc / argv enable input arguments
 	char* pDATA   = argv[1];
 	char* pCFG    = argv[2];
 	char* pWEIGHT = argv[3];
-	char* sub_name= argv[4];
-	char* sub_img = argv[5];
-	char* pub_res = argv[6];
+	char* arg_subname = argv[4];
+	char* arg_subimg  = argv[5];
+	char* arg_pubres  = argv[6];
+
+
 
 
   // initialize node, etc.
   ros::init(argc,argv,"ai_darknet_node");
   ros::NodeHandle nh;
-  ros::Publisher pub_str = nh.advertise<std_msgs::String>("detector_result",10);
 
-	image_transport::ImageTransport it(nh);
-	image_transport::Subscriber sub = it.subscribe("/images",1,imageCallback);
+  // setup subscriber for image
+  image_transport::ImageTransport it(nh);
+  image_transport::Subscriber sub_img = it.subscribe(arg_subimg,1,call_image);
 
+  // setup subscriber for imgname
+  ros::Subscriber sub_name = nh.subscribe(arg_subname,1,call_name);
+
+  // setup publisher for result
+  pub_res = nh.advertise<std_msgs::String>(arg_pubres,10);
 
 
 
@@ -93,18 +118,22 @@ int main(int argc, char **argv){ //argc / argv enable input arguments
   ros::Rate loop_rate(10);
   int count = 0;
 
-
-
-  while(ros::ok() && count < 5){
-    std_msgs::String msg;
-    std::stringstream ss;
-    ss << "hello world " << count;
-    msg.data = ss.str();
-    ROS_INFO("%s",msg.data.c_str());
-    pub_str.publish(msg);
+  std::cout<<"starting node...\n";
+  while(ros::ok()){
     ros::spinOnce();
-    loop_rate.sleep();
-    ++count;
-  }//while loop
+  }//whileloop
+
+
+//  while(ros::ok() && count < 5){
+//    std_msgs::String msg;
+//    std::stringstream ss;
+//    ss << "imgname," << count;
+//    msg.data = ss.str();
+//    ROS_INFO("%s",msg.data.c_str());
+////    pub_str.publish(msg); // don't publish just yet
+//    ros::spinOnce();
+//    loop_rate.sleep();
+//    ++count;
+//  }//while loop
   return 0;
 }//int main
