7acba8ffdd2be91a228b0d58cf5bcefdd9d03f05
Nitin Nayak <nn-171983@hs-weingarten.de>
2018-10-18 11:08:58 +0200

    Read path for positive images completed

--- trad_hog_alg/src/trad_hog_alg.py
+++ trad_hog_alg/src/trad_hog_alg.py
@@ -6,22 +6,29 @@ Objective: Cone detection using Histogram of Gradients (HOG) with traditional ma
 '''
 
 ## 1. INITIALIZATIONS
-#import rospy
+import rospy
 #import time
 import cv2
-#from sys import argv
+from sys import argv
 import numpy as np
 import os
 #from cv_bridge import CvBridge
-#from sensor_msgs.msg import Image
-#from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from std_msgs.msg import String
 
 #from skimage.feature import hog
 #from skimage import data, exposure
 import common_tools.lib_tools as lib                            # self-developed library of functions
 from imutils.object_detection import non_max_suppression        # used for NMS of rectangles after detection
 
-
+## 2. READ PATH OF POSITIVE IMAGES
+training_path_positive = argv[1]
+if(not os.path.exists(training_path_positive)):
+    print "Error, path below doesn't exist."
+else:
+    print 'Success: read training_path found.'
+os.chdir(training_path_positive)
+print 'Located at:',os.getcwd()
 
 
 
@@ -33,39 +40,13 @@ from imutils.object_detection import non_max_suppression        # used for NMS o
 fd, hog_image = hog(image, orientations=8, pixels_per_cell=(4, 4),
                     cells_per_block=(1, 1), visualize=True, multichannel=True)
 '''
-# Method 2: Use cv2 inbuilt HOG descriptor but cannot visualize
-hog = cv2.HOGDescriptor()
-hog_features = []
 
-'''
-Depending on input: needs a for loop that trains the hog or directly evaluates the incoming images:
-res = cv2.resize(cone, (64, 128))
-    h = hog.compute(res)
-    #print np.shape(hog_features)
-    hog_features.append(h)
-'''
-
-# Train SVM
-# NN_NOTE: IS the node already supposed to be trained on a training set or split the incoming set?
-samples = np.array(np.random.random((4,2)), dtype = np.float32)
-y_train = np.array([1.0,0.0,0.0,1.0], dtype = np.int32)
-
-clf = cv2.ml.SVM_create()
-clf.setKernel(cv2.ml.SVM_LINEAR)
-clf.setType(cv2.ml.SVM_C_SVC)
-clf.setC(2.67)
-clf.setGamma(5.383)
-
-clf.train(samples, cv2.ml.ROW_SAMPLE, y_train)
-
-y_val = clf.predict(samples)
 
 # NN_NOTE: ROS: Still subscribing to /images and /image_name nodes
 rospy.init_node('hog_algorithm_node')
-rospy.Subscriber('images',Image,call_img,queue_size=10)
-rospy.Subscriber('image_name',String,call_name,queue_size=10)
+#rospy.Subscriber('images',Image,call_img,queue_size=10)
+#rospy.Subscriber('image_name',String,call_name,queue_size=10)
 pub_result = rospy.Publisher('detector_result',String,queue_size=10)
-
 
 while(not rospy.is_shutdown()):
 	rospy.spin()
