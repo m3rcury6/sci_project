a16e54d1837d2a5947dcedab2aecbc8e04535f12
Nitin Nayak <nn-171983@hs-weingarten.de>
2018-10-23 00:23:16 +0200

    Final code. Debugging to be done.

--- trad_hog_alg/src/trad_hog_alg.py
+++ trad_hog_alg/src/trad_hog_alg.py
@@ -12,6 +12,7 @@ import cv2
 from sys import argv
 import numpy as np
 import os
+from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 from std_msgs.msg import String
 from imutils.object_detection import non_max_suppression
@@ -20,104 +21,49 @@ from ai_darknet.msg import bbox
 from ai_darknet.msg import bbox_array
 import time
 
-'''
-## 2. Move to testing folder                                    # not needed when test images are provided by ROS node
-testing_path = argv[1]
-if(not os.path.exists(testing_path)):
-    print "Error, path below doesn't exist."
-else:
-    print 'Success: read training_path found.'
-os.chdir(testing_path)
-print 'Located at:',os.getcwd()
-'''
-## 3. Load .xml file of SVM classifier
+## 2. Load .xml file of SVM classifier
 path = argv[1]
 if(not os.path.exists(path)):
     print "Error, path below doesn't exist."
 else:
-    print 'Success: read training_path found.'
+    print 'Success: path found.'
 os.chdir(path)
 clf = cv2.ml.SVM_load('../HOG_svm_model.xml')
-print 'Loaded file'
+print 'SVM Classifier .xml file successfully loaded'
 
-'''
-## 4.  Read a test image, resize it, save a copy                # not needed when test images are provided by ROS node
-img = cv2.imread('01_12.jpg')
-img = cv2.resize(img, (0,0), fx=2, fy=2)
-orig = img.copy()
-'''
-
-## 5. Extract support vectors from SVm
+## 3. Extract support vectors from SVM and set them to HOG descriptor
 svm = clf.getSupportVectors()
 svm = np.transpose(svm)
-
-## 6. Set the SVm to a HOG descriptor
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(svm)
+print "SVM ready!!!"
 
-## 7. Detect cones in the image
-#(rects, weights) = hog.detectMultiScale(img, winStride=(16, 16), padding=(0, 0), scale=1.2, finalThreshold = 1)
-#print('LENGTH: ',len(rects))
 
-'''
-## 8. Draw the detected bounding boxes
-for (x, y, w, h) in rects:
-    cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
-'''
-## 9. Apply Non Max Suppression
-#rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-#pick = non_max_suppression(rects, probs=None, overlapThresh=0.0)
-'''
-## 10. Draw NMS bounding boxes
-for (xA, yA, xB, yB) in pick:
-    cv2.rectangle(img, (xA, yA), (xB, yB), (255, 0, 0), 2)
-'''
-## 11. Resize images back to original sizes
-#orig = cv2.resize(orig, (0,0), fx=0.5, fy=0.5)
-#mg = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
-
-'''
-## 12. Show the images
-cv2.imshow('Test Image',orig)
-cv2.imshow("After NMS", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-'''
-
+## 4. The ROS loop thing
 imgname = ""
-img = []
 
 def image_callback(data):
-    global img
-    img = data
+    global dnet
+    img = CvBridge().imgmsg_to_cv2(data,"bgr8")                                                                         # reconvert to cv2
+    img = cv2.resize(img, (0,0), fx=2, fy=2)                                                                            # resize for better detection
+    orig = img.copy()                                                                                                   #save copy - MAY BE REMOVED
+    (rects, weights) = hog.detectMultiScale(img, winStride=(16, 16), padding=(0, 0), scale=1.2, finalThreshold = 1)     # detect the cones
+    num_of_detections = len(rects)                                                                                      # number of cones detected (very high false positives)
+    print num_of_detections                                                                                             # MAY BE REMOVED
+    rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])                                                  # prepare for NMS
+    pick = non_max_suppression(rects, probs=None, overlapThresh=0.0)                                                    # perform NMS
+    #pick = cv2.resize(pick, (0,0), fx=0.5, fy=0.5)                                                                     # resize back to original size
+    dnet = lb.pixx2dnet(pick, np.shape(pick))                                                                           # convert pixels to dnet format
+    publish_bbox(dnet)                                                                                                  # publish dnet as bbox
 
 def image_name_callback(data):
     global imgname
     imgname = data
-
-
-## Initialise ROS node and run it
-rospy.init_node('hog_algorithm_node')
-print "HELLO"
-rospy.Subscriber('images', Image, image_callback, queue_size=10)
-rospy.Subscriber('image_name', String, image_name_callback, queue_size=10)
-
-#pub_bboxes = rospy.Publisher('bboxes',bbox_array,queue_size=10)
-#pub_result = rospy.Publisher('detector_result',String,queue_size=10)
-
-while (not rospy.is_shutdown()):
-    rospy.spin()
-
-'''
-i=0
-while (not rospy.is_shutdown()):
-    imgname = 'img_'+lb.pad(str(i),3,'0')+'.jpg'; i+=1
     print 'publishing:',imgname
 
+def publish_bbox(para):
     msgs = bbox_array() # generate new, empty custom message array
-    # will publish 3 random bboxes each loop
-    bb = np.random.rand(4,4) # dummy data
-    for irow in bb:
+    for irow in para:
         imsg = bbox() # generate new, empty custom message for each bbox
         imsg.x = irow[0]
         imsg.y = irow[1]
@@ -128,7 +74,16 @@ while (not rospy.is_shutdown()):
     # forloop conversion
     msgs.header.frame_id = imgname # IMPORTANT: TRANSMIT IMAGE NAME HERE
     pub_bboxes.publish(msgs) # when ready, publish custom message array
+    print "published!!"
+
+## Initialise ROS node and run it
+rospy.init_node('hog_algorithm_node')
+print "*** NODE INTIIALISED ***"
+print "RUN ALG_TESTER NODE"
+rospy.Subscriber('images', Image, image_callback, queue_size=10)
+rospy.Subscriber('image_name', String, image_name_callback, queue_size=10)
+pub_bboxes = rospy.Publisher('bboxes',bbox_array,queue_size=10)
 
 
-    time.sleep(1)
-'''
+while (not rospy.is_shutdown()):
+    rospy.spin()
